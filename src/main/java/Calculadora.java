a0ba9c63f4f5c4691165c41fa6ebf0a2202054c6
Santiago C <santiago.ci9619@gmail.com>
2022-03-15 20:26:47 -0500

    Multiplicación terminada. División con bugs

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,4 +1,5 @@
 import java.util.function.BiFunction;
+import java.util.stream.IntStream;
 
 public class Calculadora
 {
@@ -8,12 +9,31 @@ public class Calculadora
 
         System.out.println("Suma: " + sumar.apply(num1, num2));
         System.out.println("Resta: " + restar.apply(num1, num2));
+        System.out.println("Multiplicación: " + multir.apply(num1, num2));
 
     }
 
     public static BiFunction<Integer, Integer, Integer> sumar = (num1, num2) -> num1 + num2;
 
     public static BiFunction<Integer, Integer, Integer> restar = (num1, num2) -> num1 - num2;
+
+    public static BiFunction<Integer, Integer, Integer> multir = (num1, num2) ->
+    {
+        return IntStream.range(0, num2+1)
+                .reduce((acumulador, numero) -> sumar.apply(acumulador, num1))
+                .getAsInt();
+    };
+
+
+    //TODO terminar bien el algoritmo ya que no funciona correctamente
+    public static BiFunction<Integer, Integer, Integer> div = (numerador, denominador) ->
+    {
+        return IntStream.range(0, denominador > numerador ? denominador+1: numerador+1)
+                .reduce((acumulador,numero)->
+                        multir.apply(numero,denominador) < numerador ? acumulador+1 : acumulador
+
+                ).getAsInt();
+    };
 
 
 }
